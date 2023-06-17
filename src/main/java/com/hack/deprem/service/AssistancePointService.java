2406cfbcc64d672825d69e16ba534991bcd697bf
Halim <halimmalcok@gmail.com>
2023-06-18 00:27:47 +0300

    ...

--- src/main/java/com/hack/deprem/service/AssistancePointService.java
+++ src/main/java/com/hack/deprem/service/AssistancePointService.java
@@ -3,6 +3,7 @@ package com.hack.deprem.service;
 import com.hack.deprem.dto.AssistancePointDto;
 import com.hack.deprem.dto.request.CreateAssistancePointRequest;
 import com.hack.deprem.model.AssistancePoint;
+import com.hack.deprem.model.Product;
 import com.hack.deprem.repository.AssistancePointRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,30 @@ public class AssistancePointService {
         return AssistancePointDto.convert(assistancePointRepository.getAssistancePointByLocation(location));
     }
 
+    protected void updateOnRoad(String location, Product product){
+        AssistancePoint assistancePoint = assistancePointRepository.getAssistancePointByLocation(location);
+        List<Product> onRoad = assistancePoint.getOnRoad();
+        onRoad.add(product);
+        AssistancePoint updatedPoint = new AssistancePoint(assistancePoint.getUuid(),
+                assistancePoint.getLocation(),assistancePoint.getStock(),onRoad, assistancePoint.getNeed());
+        assistancePointRepository.save(updatedPoint);
+    }
 
+    protected void updateNeed(String location, Product product){
+        AssistancePoint assistancePoint = assistancePointRepository.getAssistancePointByLocation(location);
+        List<Product> need = assistancePoint.getNeed();
+        need.add(product);
+        AssistancePoint updatedPoint = new AssistancePoint(assistancePoint.getUuid(),
+                assistancePoint.getLocation(),assistancePoint.getStock(), assistancePoint.getOnRoad(), need);
+        assistancePointRepository.save(updatedPoint);
+    }
 
+    protected void updateStock(String location, Product product){
+        AssistancePoint assistancePoint = assistancePointRepository.getAssistancePointByLocation(location);
+        List<Product> stock = assistancePoint.getStock();
+        stock.add(product);
+        AssistancePoint updatedPoint = new AssistancePoint(assistancePoint.getUuid(),
+                assistancePoint.getLocation(),stock,assistancePoint.getStock(), assistancePoint.getNeed());
+        assistancePointRepository.save(updatedPoint);
+    }
 }
